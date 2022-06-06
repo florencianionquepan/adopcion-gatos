514dd6e1602ebfec9d5347d353cf983364bb7a99
florencianionquepan <85314154+florencianionquepan@users.noreply.github.com>
2022-06-06 12:20:09 -0300

    Se oculta el div detalles cuando se muestra el div. de iniciar sesion

--- index.js
+++ index.js
@@ -87,6 +87,7 @@ const $acceder=document.querySelector('#acceder');
 $acceder.onclick=function(){
     document.querySelector('#destacados').className='oculto';
     document.querySelector('#contenido').className='oculto';
+    document.querySelector('#detalles').className='oculto';
     document.querySelector('#div-acceder').className='container-md w-50 justify-content-center';
 }
 
